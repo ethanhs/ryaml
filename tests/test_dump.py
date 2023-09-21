890ddb8012a988faf0b4e9a8e041c5b60214d779
Igor Sadchenko <igor.sadchenko@gmail.com>
2023-09-22 00:28:21 +0300

    fix tests

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -3,19 +3,19 @@ import ryaml
 def test_dump_none(yaml_file):
     ryaml.dump(yaml_file, None)
     yaml_file.seek(0)
-    assert yaml_file.read() == '---\n~\n'
+    assert yaml_file.read() == "null\n"
 
 def test_dump_key(yaml_file):
     ryaml.dump(yaml_file, { 'key': None })
     yaml_file.seek(0)
-    assert yaml_file.read() == '---\nkey: ~\n'
+    assert yaml_file.read() == 'key: null\n'
 
 def test_dump_key_value(yaml_file):
     ryaml.dump(yaml_file, { 'key': 4 })
     yaml_file.seek(0)
-    assert yaml_file.read() == '---\nkey: 4\n'
+    assert yaml_file.read() == 'key: 4\n'
 
 def test_dump_key_sequence(yaml_file):
     ryaml.dump(yaml_file, { 'key': [4, 5] })
     yaml_file.seek(0)
-    assert yaml_file.read() == '---\nkey:\n  - 4\n  - 5\n'
+    assert yaml_file.read() == 'key:\n- 4\n- 5\n'
